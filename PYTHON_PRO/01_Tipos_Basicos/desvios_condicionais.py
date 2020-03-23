18e11c1a9ff600363bfb0cc3d096fd68a11aaf4d
Diogo Miyake <akio.miyake@live.com>
2020-03-23 09:18:29 -0300

    Inserted Files
    Files inserted at Repo

---
+++ PYTHON_PRO/01_Tipos_Basicos/desvios_condicionais.py
@@ -0,0 +1,26 @@
+# Tipos Básicos
+# Desvios condicionais
+
+idade = 15 # variavel idade
+
+# Condicional se
+if idade < 18:
+    print(f"A idade é {idade}, portanto, menor que 18")
+
+# uso do Se, Então
+if idade > 18:
+    print(idade)
+elif idade < 60:
+    print(idade)
+
+# uso do se , então, senão
+idade = 65
+
+if idade < 18:
+    print(17)
+elif idade <60:
+    print(idade)
+else:
+    print(idade)
+
+
